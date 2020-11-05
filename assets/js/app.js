79eb1ef21e7d2092ed0495da8359d27ab9057937
Julien QUENTIER <j.quentier@gmail.com>
2020-11-05 18:14:59 +0100

    Création du header | import image hero

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$('.menu__icon').click(function(){
+  $('.menu__icon--one').toggleClass('active')
+  $('.menu__icon--two').toggleClass('active')
+    $('.menu__icon--three').toggleClass('active')
+    $('.nav__list').toggleClass('active')
+});
