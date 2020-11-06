05d39936d0d9b0a5281c66bc2d606b22af3c6b9c
Julien QUENTIER <j.quentier@gmail.com>
2020-11-06 11:33:15 +0100

    Création de la partie about

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,13 @@
-$('.menu__icon').click(function(){
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+$('.menu__icon').click(function () {
   $('.menu__icon--one').toggleClass('active')
   $('.menu__icon--two').toggleClass('active')
     $('.menu__icon--three').toggleClass('active')
     $('.nav__list').toggleClass('active')
     $('.hero .hero__logo').toggleClass('active')
 });
+
