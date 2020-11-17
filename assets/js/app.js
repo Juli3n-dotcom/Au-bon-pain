bc904669beb6bbd0488339e95c1e5ebda6a0688a
Julien QUENTIER <j.quentier@gmail.com>
2020-11-17 16:37:50 +0100

    Modification mineur

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,5 +15,6 @@ $('.nav__list--items a').click(function () {
   $('.menu__icon--one').toggleClass('active')
   $('.menu__icon--two').toggleClass('active')
     $('.menu__icon--three').toggleClass('active')
-    $('.nav__list').toggleClass('active')
+  $('.nav__list').toggleClass('active')
+  $('.hero .hero__logo').toggleClass('active')
 })
